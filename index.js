d3f5e3ef6ec8a0a40d95396110d2ee8e6b67536e
shibinjohn88 <shibinjohnpdlm@gmail.com>
2022-10-04 20:22:27 -0400

    refactor code to walk directions using asynchronous promise

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 // Create the playable character
-const pc = newPlayableCharacter(100, 110)
+const pc = newPlayableCharacter(100, 110);
 
 // Create a non-playable character
-const npc = newNonPlayableCharacter(50, 300)
+const npc = newNonPlayableCharacter(50, 300);
 
 // have the NPC start walking east immediately
-npc.walkEast()
+
+npc
+  .walkNorth(1400)
+  .then(() => npc.walkEast(1200))
+  .then(() => npc.walkSouth(300))
+  .then(() => npc.walkEast(1500))
+  .then(() => npc.walkSouth(1500))
+  .then(() => npc.walkWest(2700))
+  .then(() => npc.walkNorth(400));
 
 // Create the inventory
-const inventory = newInventory()
-move(inventory).to(0, 0)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
 // Create everything else
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
